787039b8b340708d30b965d403ca275bc0a6bd64
Yara Cordero Ortiz <142780499+yaracorder0@users.noreply.github.com>
2025-04-14 09:46:21 +0200

    Add files via upload

--- KN-M-04/SubDocument.js
+++ KN-M-04/SubDocument.js
@@ -3,28 +3,35 @@ const { MongoClient } = require('mongodb');
 async function runSubdocumentQueries() {
     const uri = "mongodb://admin:12345@54.160.69.208:27017/?authSource=admin&readPreference=primary&ssl=false";
     const client = new MongoClient(uri);
-    await client.connect();
-    const db = client.db("bookclub");
 
-    const subdocResult1 = await db.collection("books").aggregate([
-        { $project: { _id: 0, title: 1, reviews: 1 } }
-    ]).toArray();
-    console.log(subdocResult1);
+        await client.connect();
+        const db = client.db("bookclub");
+		
 
-    const subdocResult2 = await db.collection("books").aggregate([
-        { $match: { "reviews.rating": { $gte: 4 } } },
-        { $project: { title: 1, "reviews.rating": 1, "reviews.comment": 1 } }
-    ]).toArray();
-    console.log(subdocResult2);
+        // Query 1: Get all books with full reviews (no _id)
+        const subdocResult1 = await db.collection("books").aggregate([
+            { $project: { _id: 0, title: 1, reviews: 1 } }
+        ]).toArray();
+        console.log(subdocResult1);
 
-    const subdocResult3 = await db.collection("books").aggregate([
-        { $unwind: "$reviews" },
-        { $project: { title: 1, "reviews.rating": 1, "reviews.comment": 1 } }
-    ]).toArray();
-    console.log(subdocResult3);
-	console.log("finished successfully");
+        // Query 2: Filter books with at least one review rating >= 4
+        const subdocResult2 = await db.collection("books").aggregate([
+            { $match: { "reviews.rating": { $gte: 4 } } },
+            { $project: { _id: 0, title: 1, "reviews.rating": 1, "reviews.comment": 1 } }
+        ]).toArray();
+        console.log(subdocResult2);
 
-    await client.close();
+        // Query 3: Unwind reviews and list each separately
+        const subdocResult3 = await db.collection("books").aggregate([
+            { $unwind: "$reviews" },
+            { $project: { _id: 0, title: 1, "reviews.rating": 1, "reviews.comment": 1 } }
+        ]).toArray();
+        console.log(subdocResult3);
+
+        console.log("Finished successfully");
+
+   
+        await client.close();   
 }
 
 runSubdocumentQueries().catch(console.dir);
